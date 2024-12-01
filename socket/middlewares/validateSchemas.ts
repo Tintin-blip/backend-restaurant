6a0cc0e1154e7883ef8ae09a3e667f92344cf2a2
Tintin-blip <pertoldi18@gmail.com>
2024-12-01 19:38:08 -0400

    file with functions to handle errors in data
    validateSchema return error o null
    
    handleError. This emit error to client if there is one
    
    validateFields. function groups all validation functions and returns an error message to halt further processing.

---
+++ socket/middlewares/validateSchemas.ts
@@ -0,0 +1,21 @@
+// Middleware 
+export async function validateSchema(schema: any, data: any): Promise<string | null> {
+    const { error } = schema.validate(data);
+    return error ? error.details[0].message : null; // return null or error
+}
+
+// this function is used to emit client 
+export function handleError(socket: any, err: any): void {
+    if (err) {
+        socket.emit('server:error', err); // send error to client
+        return;
+    }
+}
+
+// function to get error, handle it and emit to client
+export async function validateFields(schema:any,data:any,socket:any):Promise <string | null | undefined>    { 
+    const errorMessage = await validateSchema(schema,data) 
+    handleError(socket,errorMessage)
+    if(errorMessage) return errorMessage;
+}
+  
